6952b10a3b91cc5824c2ff079f0b1aa1f05f9770
taycpu <77157800+taycpu@users.noreply.github.com>
2023-01-17 23:49:40 +0300

    Refactoring naming, removing unnecessary parts, improving readability.

--- Assets/Source/IOC/Base/ContainerBase.cs
+++ Assets/Source/IOC/Base/ContainerBase.cs
@@ -4,12 +4,11 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
-[System.Serializable]
+[Serializable]
 public class Container
 {
     protected Dictionary<Type, Dictionary<string, GameObject>> implementations = new Dictionary<Type, Dictionary<string, GameObject>>();
-    protected readonly Dictionary<Type, Dictionary<string, Type>>
-    types = new Dictionary<Type, Dictionary<string, Type>>();
+    protected readonly Dictionary<Type, Dictionary<string, Type>> types = new Dictionary<Type, Dictionary<string, Type>>();
     protected readonly Dictionary<Type, TypeData> typeDatas = new Dictionary<Type, TypeData>();
 
     public virtual void Register(Type interfaceType, Type type, ClassInfo info)
@@ -18,7 +17,7 @@ public class Container
         {
             TypeData typeData = TypeData.Create(type, info.isSingleton, type.IsSubclassOf(typeof(MonoBehaviour)) ? info.implementation : null);
             string key = "";
-            object implementation = new();
+            object implementation;
             if (type.IsSubclassOf(typeof(MonoBehaviour)))
             {
                 key = info.implementation.gameObject.name;
@@ -28,27 +27,27 @@ public class Container
             {
                 implementation = ConstructClass(type);
             }
-            if (this.types.ContainsKey(interfaceType ?? type))
+            if (types.ContainsKey(interfaceType ?? type))
             {
                 var regex = new Regex($"{key}[0-9]*$");
-                if (types[interfaceType ?? type].ContainsKey(key ?? string.Empty)) key = key + types[interfaceType ?? type].Count(type => regex.IsMatch(type.Key));
-                this.types[interfaceType ?? type].Add(key ?? string.Empty, type);
+                if (types[interfaceType ?? type].ContainsKey(key)) key = key + types[interfaceType ?? type].Count(typePointer => regex.IsMatch(typePointer.Key));
+                types[interfaceType ?? type].Add(key, type);
             }
             else
             {
-                this.types.Add(interfaceType ?? type, new Dictionary<string, Type> { { key ?? string.Empty, type } });
+                types.Add(interfaceType ?? type, new Dictionary<string, Type> { { key, type } });
             }
-            if (!this.typeDatas.ContainsKey(type))
+            if (!typeDatas.ContainsKey(type))
             {
-                this.typeDatas.Add(type, typeData);
+                typeDatas.Add(type, typeData);
             }
-            if (!this.implementations.ContainsKey(type))
+            if (!implementations.ContainsKey(type))
             {
-                this.implementations.Add(type, new Dictionary<string, GameObject> { { key ?? string.Empty, implementation as GameObject} });
+                implementations.Add(type, new Dictionary<string, GameObject> { { key, implementation as GameObject} });
             }
             else
             {
-                this.implementations[type].Add(key ?? string.Empty, implementation as GameObject);
+                implementations[type].Add(key, implementation as GameObject);
             }
 
             
@@ -61,7 +60,7 @@ public class Container
 
     public T Inject<T>(object obj)
     {
-        return (T)this.Inject(typeof(T), obj);
+        return (T)Inject(typeof(T), obj);
     }
 
     private object ConstructClass(Type type)
@@ -77,41 +76,41 @@ public class Container
 
     private TypeData GetTypeData(Type type)
     {
-        if (!this.typeDatas.ContainsKey(type))
+        if (!typeDatas.ContainsKey(type))
         {
             var typeData = TypeData.Create(type);
-            this.typeDatas.Add(type, typeData);
-            if (this.types.ContainsKey(type))
+            typeDatas.Add(type, typeData);
+            if (types.ContainsKey(type))
             {
-                this.types[type].Add(string.Empty, type);
+                types[type].Add(string.Empty, type);
             }
             else
             {
-                this.types.Add(type, new Dictionary<string, Type> { { string.Empty, type } });
+                types.Add(type, new Dictionary<string, Type> { { string.Empty, type } });
             }
         }
 
-        return this.typeDatas[type];
+        return typeDatas[type];
     }
 
     public object Inject(Type type, object obj)
     {
-        var typeData = this.GetTypeData(type);
+        var typeData = GetTypeData(type);
         
-        typeData.Fields.ForEach(x => x.Value.SetValue(obj, this.Resolve(x.Value.FieldType, string.IsNullOrEmpty(x.Key.Key) ? GetImplementation(x.Value.FieldType).gameObject.name : x.Key.Key)));
-        typeData.Properties.ForEach(x => x.Value.SetValue(obj, this.Resolve(x.Value.PropertyType, x.Key.Key), null));
+        typeData.Fields.ForEach(x => x.Value.SetValue(obj, Resolve(x.Value.FieldType, string.IsNullOrEmpty(x.Key.Key) ? GetImplementation(x.Value.FieldType).gameObject.name : x.Key.Key)));
+        typeData.Properties.ForEach(x => x.Value.SetValue(obj, Resolve(x.Value.PropertyType, x.Key.Key), null));
 
         return obj;
     }
 
     public object Resolve(Type type, string key = null)
     {
-        Guard(!this.types[type.BaseType].ContainsKey(key ?? string.Empty),
+        Guard(!types[type.BaseType].ContainsKey(key ?? string.Empty),
             "There is no implementation registered with the key {0} for the type {1}.", key, type.Name);
 
-        var foundType = this.types[type.BaseType][key ?? string.Empty];
-        var implementationOfType = this.implementations[type][key ?? string.Empty].GetComponent(type) ?? Inject(foundType, implementations[type][key ?? string.Empty].AddComponent(foundType));
-        var typeData = this.typeDatas[foundType];
+        var foundType = types[type.BaseType][key ?? string.Empty];
+        var implementationOfType = implementations[type][key ?? string.Empty].GetComponent(type) ?? Inject(foundType, implementations[type][key ?? string.Empty].AddComponent(foundType));
+        var typeData = typeDatas[foundType];
 
         if (foundType.IsSubclassOf(typeof(MonoBehaviour)))
         {
@@ -121,36 +120,36 @@ public class Container
 
         if (typeData.IsSingleton)
         {
-            return typeData.Instance ?? (typeData.Instance = this.Setup(foundType));
+            return typeData.Instance ?? (typeData.Instance = Setup(foundType));
         }
 
-        return this.Setup(foundType);
+        return Setup(foundType);
     }
     private object Setup(Type type)
     {
         var instance = Activator.CreateInstance(type);
-        this.Inject(type, instance);
+        Inject(type, instance);
         return instance;
     }
 
-    private Component GetImplementation(Type type, string key = null, GameObject sibling = null)
+    private Component GetImplementation(Type type, string key = null)
     {
-        Guard(!this.implementations.ContainsKey(type), "There is no implementation of {0} for the {1}", type.Name, key ?? string.Empty);
+        Guard(!implementations.ContainsKey(type), "There is no implementation of {0} for the {1}", type.Name, key ?? string.Empty);
         GameObject imp;
         if (string.IsNullOrEmpty(key))
         {
-            imp = this.implementations[type].FirstOrDefault().Value;
+            imp = implementations[type].FirstOrDefault().Value;
         }
         else
         {
-            imp = this.implementations[type][key];
+            imp = implementations[type][key];
         }
         var implementationOfType = imp.GetComponent(type);
         return implementationOfType;
     }
 
 }
-[System.Serializable]
+[Serializable]
 public struct ClassInfo
 {
     public MonoBehaviour implementation;
